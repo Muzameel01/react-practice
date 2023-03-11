70d750587d3dd6c3ab0655a6aaac7cca0745392d
Muzameel01 <mkmujameel@gmail.com>
2023-03-11 09:29:54 +0530

    feat: practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PakagingList from './PakagingList';
+import ListRender from './ListRender';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PakagingList/>
+        {/* <PakagingList/> */}
+        <ListRender/>
       </header>
     </div>
   );
