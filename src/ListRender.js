f3464f2f1e8ab0b1362616509a3e677df1d0eb9b
Muzameel01 <mkmujameel@gmail.com>
2023-03-13 09:32:32 +0530

    feat:practical

--- src/ListRender.js
+++ src/ListRender.js
@@ -9,9 +9,10 @@ import {getImageUrl} from "./utils";
 import { person } from "./data";
 
 export default function ListRender(){
-    // const chemist = person.filter((p)=>p.profession==="chemist");
+     const chemist = person.filter((p)=>p.profession==="chemist");
+     const everyOneElse= person.filter((p)=>p.profession!=="chemist")
     // const listItem = person.map((person)=>(<li key={person.id}>{person.name}:{person.profession}</li>));
-    const listItem = person.map(person =>
+    const chemistlistItem = chemist.map(person =>
         <li>
           <img
             src={getImageUrl(person)}
@@ -24,15 +25,32 @@ export default function ListRender(){
           </p>
         </li>
       );
-    return(
+      const everyOneElselistItem = everyOneElse.map(person =>
+        <li>
+          <img
+        src={getImageUrl(person)}
+        alt={person.name}
+          />
+          <p>
+            <b>{person.name}:</b>
+            {' ' + person.profession + ' '}
+            known for {person.accomplishment}
+          </p>
+        </li>
+      );
+    return<>
+      <h1>Chemist</h1>
         <ul>
-           {listItem}</ul>  
-//         <ul>
+           {chemistlistItem}</ul> 
+           <h1>Others</h1>
+           <ul>
+            {everyOneElselistItem}</ul> 
+{/* //         <ul>
 //             <li>Creola Katherine Johnson: mathematician</li>
 //   <li>Mario José Molina-Pasquel Henríquez: chemist</li>
 //   <li>Mohammad Abdus Salam: physicist</li>
 //   <li>Percy Lavon Julian: chemist</li>
 //   <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
-//         </ul>
-    )
+//         </ul> */}
+</>
 }
